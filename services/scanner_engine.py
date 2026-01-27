b6271803e1df5e58862573e9388078a3163cee76
saishtradingbeliveintrade <tradermarathi07@gmail.com>
2026-01-27 23:45:09 +0530

    Update scanner_engine.py

--- services/scanner_engine.py
+++ services/scanner_engine.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from datetime import datetime
-from instrument_loader import INSTRUMENT_MAP
+from services.instrument_map import INSTRUMENT_MAP
 
 ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
 
@@ -15,11 +15,10 @@ INTRADAY_THRESHOLD = 0.8
 
 
 def get_ltp_bulk():
-    keys = list(INSTRUMENT_MAP.values())
     url = "https://api.upstox.com/v3/market-quote/ltp"
-    params = {"instrument_key": ",".join(keys)}
+    params = {"instrument_key": ",".join(INSTRUMENT_MAP.values())}
 
-    res = requests.get(url, headers=HEADERS, params=params, timeout=10)
+    res = requests.get(url, headers=HEADERS, params=params, timeout=15)
     data = res.json().get("data", {})
 
     prices = {}
@@ -31,16 +30,22 @@ def get_ltp_bulk():
 
 def get_5min_candle(instrument_key):
     url = f"https://api.upstox.com/v3/historical-candle/intraday/{instrument_key}/minutes/5"
-    res = requests.get(url, headers=HEADERS, timeout=10)
-    candles = res.json()["data"]["candles"]
-    return candles[-1] if candles else None
+    try:
+        res = requests.get(url, headers=HEADERS, timeout=10)
+        candles = res.json()["data"]["candles"]
+        return candles[-1] if candles else None
+    except:
+        return None
 
 
 def get_day_open(instrument_key):
     url = f"https://api.upstox.com/v3/historical-candle/intraday/{instrument_key}/days/1"
-    res = requests.get(url, headers=HEADERS, timeout=10)
-    candles = res.json()["data"]["candles"]
-    return candles[-1][1] if candles else None
+    try:
+        res = requests.get(url, headers=HEADERS, timeout=10)
+        candles = res.json()["data"]["candles"]
+        return candles[-1][1] if candles else None
+    except:
+        return None
 
 
 def scan_market():
