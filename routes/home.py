7892e40fad3632490d58ebcfb82204504ca992b7
saishtradingbeliveintrade <tradermarathi07@gmail.com>
2026-01-27 19:34:51 +0530

    Update home.py

--- routes/home.py
+++ routes/home.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from services.market_data import get_multiple_ltp
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    symbols = ["RELIANCE", "TCS", "INFY", "HDFCBANK", "ITC", "SBIN"]
+    stocks = get_multiple_ltp(symbols)
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "stocks": stocks
+    })
