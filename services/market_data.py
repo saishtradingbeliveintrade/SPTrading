6d0fb8a897c01a482a1af020f49c07f41e8f24cc
saishtradingbeliveintrade <tradermarathi07@gmail.com>
2026-01-27 18:52:09 +0530

    Update market_data.py

--- services/market_data.py
+++ services/market_data.py
@@ -1,33 +1,19 @@
 import requests
 from config import UPSTOX_ACCESS_TOKEN
+from instrument_map import INSTRUMENT_MAP   # <- हा file वेगळा ठेव
 
 HEADERS = {
     "Accept": "application/json",
     "Authorization": f"Bearer {UPSTOX_ACCESS_TOKEN}"
 }
 
-# RELIANCE instrument key example
-INSTRUMENT_KEY = "NSE_EQ|INE002A01018"
 
+def get_ltp(symbol: str):
+    key = INSTRUMENT_MAP.get(symbol.upper())
 
-def get_ltp():
-    url = f"https://api.upstox.com/v3/market-quote/ltp?instrument_key={INSTRUMENT_KEY}"
+    if not key:
+        return {"error": "Invalid symbol"}
+
+    url = f"https://api.upstox.com/v3/market-quote/ltp?instrument_key={key}"
     res = requests.get(url, headers=HEADERS)
     return res.json()
-
-
-def get_intraday_candle():
-    url = f"https://api.upstox.com/v3/historical-candle/intraday/{INSTRUMENT_KEY}/minutes/5"
-    res = requests.get(url, headers=HEADERS)
-    return res.json()
-
-
-def get_historical_candle():
-    url = f"https://api.upstox.com/v3/historical-candle/{INSTRUMENT_KEY}/days/1/2023-01-01/2023-12-31"
-    res = requests.get(url, headers=HEADERS)
-    return res.json()
-
-
-def get_live_data():
-    ltp = get_ltp()
-    return ltp
