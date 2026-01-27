50df427c70aa0dfad2b7f720170a1d5c0d520263
saishtradingbeliveintrade <tradermarathi07@gmail.com>
2026-01-27 19:56:10 +0530

    Update home.py

--- routes/home.py
+++ routes/home.py
@@ -2,14 +2,16 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from services.market_data import get_multiple_ltp
+from services.instrument_map import INSTRUMENT_MAP
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    symbols = ["RELIANCE", "TCS", "INFY", "HDFCBANK", "ITC", "SBIN"]
+    symbols = list(INSTRUMENT_MAP.keys())[:50]  # आत्ता 50 दाखवू (fast load साठी)
     stocks = get_multiple_ltp(symbols)
+
     return templates.TemplateResponse("index.html", {
         "request": request,
         "stocks": stocks
