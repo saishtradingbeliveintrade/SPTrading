f679fdfc6e038129c0c302f9fbe36668895b14cc
saishtradingbeliveintrade <tradermarathi07@gmail.com>
2026-01-27 18:46:40 +0530

    Update market_data.py

--- services/market_data.py
+++ services/market_data.py
@@ -1,8 +1,33 @@
-import random
+import requests
+from config import UPSTOX_ACCESS_TOKEN
+
+HEADERS = {
+    "Accept": "application/json",
+    "Authorization": f"Bearer {UPSTOX_ACCESS_TOKEN}"
+}
+
+# RELIANCE instrument key example
+INSTRUMENT_KEY = "NSE_EQ|INE002A01018"
+
+
+def get_ltp():
+    url = f"https://api.upstox.com/v3/market-quote/ltp?instrument_key={INSTRUMENT_KEY}"
+    res = requests.get(url, headers=HEADERS)
+    return res.json()
+
+
+def get_intraday_candle():
+    url = f"https://api.upstox.com/v3/historical-candle/intraday/{INSTRUMENT_KEY}/minutes/5"
+    res = requests.get(url, headers=HEADERS)
+    return res.json()
+
+
+def get_historical_candle():
+    url = f"https://api.upstox.com/v3/historical-candle/{INSTRUMENT_KEY}/days/1/2023-01-01/2023-12-31"
+    res = requests.get(url, headers=HEADERS)
+    return res.json()
+
 
 def get_live_data():
-    return [
-        {"name": "RELIANCE", "price": round(random.uniform(2400, 2600), 2)},
-        {"name": "TCS", "price": round(random.uniform(3500, 3700), 2)},
-        {"name": "INFY", "price": round(random.uniform(1400, 1500), 2)},
-    ]
+    ltp = get_ltp()
+    return ltp
